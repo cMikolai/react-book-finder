e7d24d41a2684cbd65e7bac4e0f8f0885365e3bd
Christin Mikolai <mail@christinmikolai.de>
2019-02-24 23:55:25 +0100

    add required informations (author,img, ...)

--- src/App.js
+++ src/App.js
@@ -20,14 +20,20 @@ class BookCard extends Component {
     var singleBook = this.props.books.map((book) => {
       // uses same function as Map.js to match results
       return (
-        <li>
-          {book.volumeInfo.title}
+        <li key={book.volumeInfo.title} className="book-card">
+          <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
+          <h2>{book.volumeInfo.title}</h2>
+          <p>by {book.volumeInfo.authors}</p>
+          <p>Published by {book.volumeInfo.publisher}</p>
+          <a href={book.volumeInfo.previewLink}>
+            <button >See this Book</button>
+          </a>
         </li>
       )
     })
 
     return (
-        <div>
+      <div>
         {singleBook}
       </div>
 
