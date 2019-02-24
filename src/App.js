94ac31c51af90862984ab9b97365a55ae031170a
Christin Mikolai <mail@christinmikolai.de>
2019-02-24 21:35:55 +0100

    creates basic App structure/components

--- src/App.js
+++ src/App.js
@@ -2,24 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Search extends Component {
+  render() {
+    return(
+      <div className="App-search">
+        <form>
+          <label for="booksearch">Search book:</label>
+          <input type="text" name="name" placeholder="Search for book" id="booksearch" />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+class BookCard extends Component {
+  render() {
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-header">
+          <h1>Book Finder</h1>
+          <Search />
+        </div>
+
+        <BookCard />
       </div>
     );
   }
