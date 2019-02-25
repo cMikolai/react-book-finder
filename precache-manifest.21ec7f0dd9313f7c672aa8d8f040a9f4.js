27e047674d7024ad1bbd4b5c69dd10827eb151cb
Christin Mikolai <mail@christinmikolai.de>
2019-02-25 20:55:45 +0100

    Updates

---
+++ precache-manifest.21ec7f0dd9313f7c672aa8d8f040a9f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c2d114dc2dd1476b590",
+    "url": "/react-book-finder/static/js/runtime~main.4c2d114d.js"
+  },
+  {
+    "revision": "d4794cf382b7a5a0ee94",
+    "url": "/react-book-finder/static/js/main.d4794cf3.chunk.js"
+  },
+  {
+    "revision": "9b8a3a2b03d1d385b942",
+    "url": "/react-book-finder/static/js/2.9b8a3a2b.chunk.js"
+  },
+  {
+    "revision": "d4794cf382b7a5a0ee94",
+    "url": "/react-book-finder/static/css/main.5c5ebbba.chunk.css"
+  },
+  {
+    "revision": "48a50458da91052602d6b173ee5f01fc",
+    "url": "/react-book-finder/index.html"
+  }
+];
